904ff9aa4fbb10072dfb90ab7ac6df2ea8f040cc
Faisal-Hasan <77873039+Faisal-Hasan@users.noreply.github.com>
2025-08-10 16:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn=document.getElementById("btn1");
+btn.addEventListner("click",()=>{
+	 let t=document.getElementById("txt1");
+	t.outerHTML="<h1>Entered Metaverse</h1>";
+})
